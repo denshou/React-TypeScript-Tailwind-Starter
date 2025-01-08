0d73600b77f6ac97062bb57a521e63af42b0d2ba
denshou <zecaumr@naver.com>
2025-01-08 22:30:16 +0900

    react + typescript + tailwind init

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import { Route, Routes } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-import Main from "./pages/Main";
-import MovieInfo from "./components/MovieInfo";
-import { useModal } from "./stores/modalStore";
-
 function App() {
-  const movieModalOpen = useModal((state) => state.movieModalOpen);
-  return (
-    <>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Main />} />
-          <Route path="/movie/:id" element={<Main />} />
-        </Route>
-      </Routes>
-      {movieModalOpen && <MovieInfo/>}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/components/DetailInfo.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function DetailInfo() {
-  return (
-    <div>DetailInfo</div>
-  )
-}
